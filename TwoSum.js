10733a8cf284921ca646e86a67f114d8261bebf9
Mayuresh Juvekar <73056290+Mayureshju@users.noreply.github.com>
2024-01-13 20:02:00 +0530

    Update TwoSum.js

--- TwoSum.js
+++ TwoSum.js
@@ -30,3 +30,15 @@ var twoSum = function(nums, target) {
 
 solution 2:
 
+
+var twoSum = function(nums, target) {
+   var obj = {}
+   for(let i=0;i<nums.length;i++){
+let n = nums[i];
+if(obj[target-n] >= 0){
+return [obj[target-n],i]
+}else{
+    obj[n] = i;
+}
+   }
+};
